88732b58d256627472026133ac7dcf6963f2aa97
Ahmad Hazrati <hazrati.ahmad@gmail.com>
2023-09-24 10:05:13 +0000

    Update heroku hostname link in the settings.py

--- thinkgreen/settings.py
+++ thinkgreen/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8000-ahmadhazrat-djangothink-b7in4x661gy.ws-eu104.gitpod.io']
+ALLOWED_HOSTS = ['thinkgreen-app-358b81a3a5f3.herokuapp.com', '8000-ahmadhazrat-djangothink-b7in4x661gy.ws-eu104.gitpod.io']
 
 
 # Application definition
